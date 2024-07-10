a00cf2a19bd49867bb1599c129791202f0c78f14
Vlad <git>
2024-07-10 19:45:25 +0100

    ф

---
+++ for work/Back/api/index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const app = require("../app");
+const dotenv = require('dotenv');
+
+
+
+
+
+dotenv.config();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+
+server.listen(PORT,()=>{console.log(`server started at PORT: ${PORT}`)});
+
+module.exports = server;
