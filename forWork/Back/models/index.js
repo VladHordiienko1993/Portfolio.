9d7ba46edd0f1b9f5a6b32de1c1fd7b45bcaac64
Vlad <git>
2024-07-11 17:40:22 +0100

    add model for vercel

--- forWork/Back/models/index.js
+++ forWork/Back/models/index.js
@@ -13,7 +13,10 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    ...config,
+    dialect: 'postgres' // Явно укажите диалект здесь
+  });
 }
 
 fs
