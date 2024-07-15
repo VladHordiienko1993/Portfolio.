a677b1526f7e374f5279578a032524a77688c168
Vlad <git>
2024-07-15 21:16:13 +0100

    Added SQL scripts for Users and Theme

---
+++ forWork/Back/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email, password)
+VALUES ('Bot1','Bot1VladHordiienko@gmail.com','1adqqwergAA1');
+
+INSERT INTO users (name, email, password)
+VALUES ('Bot2','Bot2VladHordiienko@gmail.com','2adqqwergAA2');
+
+INSERT INTO themeswitches (name)
+VALUES ('theme');
