bfa2f2ed47d7e2a16fa7363a9353f9f5faf6e792
Vlad <git>
2024-09-14 17:36:49 +0100

    error session

--- forWork/Back/controllers/user.controller.js
+++ forWork/Back/controllers/user.controller.js
@@ -13,7 +13,7 @@ module.exports.userSession = async (req,res,next)=>{
       if (err) {
         console.error('Error saving session:', err);
       }
-      return res.status(200).send('Session saved successfully');
+      console.log('Session saved successfully');
     });
     const {user} = req.session;
     if (!user) {
