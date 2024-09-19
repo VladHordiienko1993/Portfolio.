79421a8d0351ddbd8393a2a1fc434b93fa5a9958
Vlad <git>
2024-09-19 14:53:32 +0100

    secure: true in session

--- forWork/Back/app.js
+++ forWork/Back/app.js
@@ -53,7 +53,7 @@ app.use(session({
   saveUninitialized: false,                       
   cookie: {
     sameSite: "none",      
-    // secure: true,              
+    secure: true,              
     httpOnly: true,       
     maxAge: 24 * 60 * 60 * 1000                    
   }
