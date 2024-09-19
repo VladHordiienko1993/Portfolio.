94d6362adf8ca8309ff0f342755bbaf4a81058b6
Vlad <git>
2024-09-19 17:44:00 +0100

    session

--- forWork/Back/app.js
+++ forWork/Back/app.js
@@ -61,7 +61,7 @@ app.use(session({
 }));
 app.use((req, res, next) => {
   console.log('Session ID:', req.sessionID);
-  console.log('Session Data:', req.session);
+  console.log('Session Data:', req.session.user);
   next();
 });
 
