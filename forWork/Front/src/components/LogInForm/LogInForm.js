9590c2c13b75c2561f88f0d39794071b28f851d3
Vlad <git>
2024-09-27 20:27:55 +0100

    changed onSubmit in loginForm

--- forWork/Front/src/components/LogInForm/LogInForm.js
+++ forWork/Front/src/components/LogInForm/LogInForm.js
@@ -14,54 +14,54 @@ const LogInForm = () => {
  
 
   
-  // const onSubmit = async(values, formikBag) => {
-  //   const {data:{data: user}} = await API.fetchLoginUser(values)
-  //         .catch((err)=>{
-  //           if(err && err.message){
-  //             dispatch(createUserError(err.request.status))
-  //           }
-  //         })
+  const onSubmit = async(values, formikBag) => {
+    const {data:{data: user}} = await API.fetchLoginUser(values)
+          .catch((err)=>{
+            if(err && err.message){
+              dispatch(createUserError(err.request.status))
+            }
+          })
 
-  //         if(user){
-  //           dispatch(createUserSuccess(user))
+          if(user){
+            dispatch(createUserSuccess(user))
 
-  //           formikBag.resetForm()
-  //           push('/')
-  //         }
-  // };
+            formikBag.resetForm()
+            push('/')
+          }
+  };
  
 
-  const onSubmit = async(values, formikBag) => {
-    try {
-      // Логин пользователя
-      const { data: { data: user } } = await API.fetchLoginUser(values);
+  // const onSubmit = async(values, formikBag) => {
+  //   try {
+  //     // Логин пользователя
+  //     const { data: { data: user } } = await API.fetchLoginUser(values);
       
-      // Если логин успешен, обновляем состояние пользователя в Redux
-      if(user) {
-        dispatch(createUserSuccess(user));
+  //     // Если логин успешен, обновляем состояние пользователя в Redux
+  //     if(user) {
+  //       dispatch(createUserSuccess(user));
   
-        // Получаем текущую сессию после успешного логина
-        const sessionResponse = await API.fetchSessionUser();
+  //       // Получаем текущую сессию после успешного логина
+  //       const sessionResponse = await API.fetchSessionUser();
   
-        // Обрабатываем успешный ответ сессии
-        dispatch(sessionSuccess(sessionResponse.data));
+  //       // Обрабатываем успешный ответ сессии
+  //       dispatch(sessionSuccess(sessionResponse.data));
   
-        // Сбрасываем форму
-        formikBag.resetForm();
+  //       // Сбрасываем форму
+  //       formikBag.resetForm();
   
-        // Редиректим пользователя на главную страницу
-        push('/');
-      }
-    } catch (err) {
-      // Обрабатываем ошибки логина
-      if (err && err.response) {
-        dispatch(createUserError(err.response.status));
-      }
+  //       // Редиректим пользователя на главную страницу
+  //       push('/');
+  //     }
+  //   } catch (err) {
+  //     // Обрабатываем ошибки логина
+  //     if (err && err.response) {
+  //       dispatch(createUserError(err.response.status));
+  //     }
   
-      // В случае ошибки сессии (опционально можно обрабатывать её отдельно)
-      dispatch(sessionError(err.response?.status || 'Ошибка при получении сессии'));
-    }
-  };
+  //     // В случае ошибки сессии (опционально можно обрабатывать её отдельно)
+  //     dispatch(sessionError(err.response?.status || 'Ошибка при получении сессии'));
+  //   }
+  // };
 
 
 
